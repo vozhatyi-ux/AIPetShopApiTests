379d7e1a2e7241788934c47d86e8ec02cecdf982
vozhatyi-ux <vozhatyi@gmail.com>
2026-01-22 01:50:24 +0300

    Добавил тест "Добавление нового питомца"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,9 +1,12 @@
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
+
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
-@allure.feature("Pet")
 
+@allure.feature("Pet")
 class TestPet:
     @allure.title("Попытка удалить несуществующего питомца")
     def test_delete_nonexistent_pet(self):
@@ -15,7 +18,6 @@ class TestPet:
 
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
-
 
     @allure.title("Попытка обновить несуществующего питомца")
     def test_update_nonexistent_pet(self):
@@ -33,4 +35,18 @@ class TestPet:
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
 
+    @allure.title("Добавление нового питомца")
+    def test_add_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "status": "available"
+            }
 
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet/", json=payload)
+
+        with allure.step("Проверка статуса ответа и вадидации JSON-схемы"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            jsonschema.validate(response.json(), PET_SCHEMA)
